782cad9c2f2b11cf978dc927765620fea4321e71
Mike Duke <aithon@gmail.com>
2019-06-13 12:20:33 -0600

    Add Header component and reset.css

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
+    <Header />
     <Aside @make-todo="addToDo" />
     <ToDoList :todos="todos" />
   </div>
 </template>
 
 <script>
-import Aside from './components/Aside.vue';
+import Header from './components/Header';
+import Aside from './components/Aside';
 import ToDoList from './components/ToDoList';
+import './assets/reset.css';
 
 export default {
   name: 'app',
   components: {
+    Header,
     Aside,
-    ToDoList
+    ToDoList,
   },
   data: () => {
     return {
@@ -29,12 +33,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>Check <span id="yo">Yo'</span> Self</h1>
+    <form>
+      <button>Search</button>
+      <input type="text" placeholder="search" v-model="searchInput" id="search-input">
+    </form>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        searchInput: ''
+      }
+    }
+  }
+</script>
+
+<style>
+  header {
+    grid-area: header;
+    background: #1F1F3D;
+  }
+
+  h1 {
+    color: #FFFFFF;
+    font-weight: 800;
+  }
+
+  #yo {
+    color: #FFC30C;
+  }
+</style>
